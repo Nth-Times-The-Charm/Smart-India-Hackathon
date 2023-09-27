18aecfa7e8b6dd28c568e2270ef3c621d742ac23
Om Mishra <admin@projectrexa.dedyn.io>
2023-09-27 13:44:10 +0000

    Fixed Issue

--- app/main.py
+++ app/main.py
@@ -378,4 +378,4 @@ def verify_domain():
         return redirect(url_for("organization_signup"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7777, debug=True)
+    app.run(host="0.0.0.0", port=7777, debug=False)
